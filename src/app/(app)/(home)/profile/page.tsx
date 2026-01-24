7e9b782b2c716d73a84161ae7774e6de245ea5a3
Christopher Haris <christoharis2005@gmail.com>
2026-01-24 12:11:14 +0700

    fix: profile force dynamic

--- src/app/(app)/(home)/profile/page.tsx
+++ src/app/(app)/(home)/profile/page.tsx
@@ -2,6 +2,8 @@ import { Button } from "@/components/ui/button";
 import { caller } from "@/trpc/server";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default async function ProfilePage() {
   const session = await caller.auth.session();
 
