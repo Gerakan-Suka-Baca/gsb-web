743768779128c32ad7eb126b4d40f2342e468663
Christopher Haris <christoharis2005@gmail.com>
2026-01-24 12:04:07 +0700

    fix: tryout getmany type

--- src/modules/tryouts/types.ts
+++ src/modules/tryouts/types.ts
@@ -2,6 +2,6 @@ import { inferRouterOutputs } from "@trpc/server";
 
 import type { appRouter } from "@/trpc/routers/_app";
 
-export type MembersGetManyOutput = inferRouterOutputs<
+export type TryoutsGetManyOutput = inferRouterOutputs<
   typeof appRouter
->["members"]["getMany"];
+>["tryouts"]["getMany"];
